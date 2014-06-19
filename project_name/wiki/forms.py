093c840faf27efe5332766d7c0c05695a38a62b2
Patrick Altman <paltman@gmail.com>
2014-06-19 16:56:20 -0500

    Add protection from the lost update problem

--- project_name/wiki/forms.py
+++ project_name/wiki/forms.py
@@ -3,13 +3,30 @@ from django import forms
 from .models import Revision
 
 
-class PageForm(forms.ModelForm):
+class RevisionForm(forms.ModelForm):
 
+    revision_pk = forms.IntegerField(widget=forms.HiddenInput())
     message = forms.CharField(required=False, help_text="Leave a helpful message about your change")
+
+    def __init__(self, *args, **kwargs):
+        super(RevisionForm, self).__init__(*args, **kwargs)
+        print self.instance, self.instance.pk
+        self.fields["revision_pk"].initial = self.instance.pk
+
+    def clean_content(self):
+        if self.cleaned_data["content"] == self.instance.content:
+            raise forms.ValidationError("You made no stinking changes")
+        return self.cleaned_data["content"]
+
+    def clean(self):
+        if self.cleaned_data["revision_pk"] != self.instance.pk:
+            raise forms.ValidationError("Someone edited this before you")
+        return self.cleaned_data
 
     class Meta:
         model = Revision
         fields = [
+            "revision_pk",
             "content",
             "message"
         ]
--- project_name/wiki/views.py
+++ project_name/wiki/views.py
@@ -2,7 +2,7 @@ from django.http import Http404, HttpResponseForbidden
 from django.shortcuts import redirect, render
 
 from .conf import settings
-from .forms import PageForm
+from .forms import RevisionForm
 from .hooks import hookset
 from .models import Page
 
@@ -29,37 +29,33 @@ def edit(request, slug, wiki_lookup, *args, **kwargs):
     try:
         page = wiki.pages.get(slug=slug)
         rev = page.revisions.latest()
-        initial = {
-            "content": rev.content
-        }
         if not hookset.can_edit_page(page, request.user):
             return HttpResponseForbidden()
     except Page.DoesNotExist:
         page = Page(wiki=wiki, slug=slug)
         rev = None
-        initial = {
-            "content": "add content and create a new page",
-            "message": "initial revision"
-        }
         if not hookset.can_edit_page(page, request.user):
             raise Http404()
     if request.method == "POST":
-        form = PageForm(request.POST)
+        form = RevisionForm(request.POST, instance=rev)
         if form.is_valid():
-            if initial["content"] == form.cleaned_data["content"]:
-                form.errors["content"] = ["You haven't made any changes!"]
-            else:
-                if page.pk is None:
-                    page.save()
-                revision = form.save(commit=False)
-                revision.page = page
-                revision.created_by = request.user
-                revision.created_ip = request.META.get(settings.WIKI_IP_ADDRESS_META_FIELD, "REMOTE_ADDR")
-                revision.parse()
-                revision.save()
-                return redirect(hookset.page_url(wiki, slug))
+            if page.pk is None:
+                page.save()
+            revision = form.save(commit=False)
+            revision.page = page
+            revision.created_by = request.user
+            revision.created_ip = request.META.get(settings.WIKI_IP_ADDRESS_META_FIELD, "REMOTE_ADDR")
+            revision.parse()
+            revision.save()
+            return redirect(hookset.page_url(wiki, slug))
     else:
-        form = PageForm(initial=initial)
+        if rev:
+            form = RevisionForm(instance=rev)
+        else:
+            form = RevisionForm(initial={
+                "content": "add content and create a new page",
+                "message": "initial revision"
+            })
 
     return render(request, "wiki/edit.html", {
         "form": form,
