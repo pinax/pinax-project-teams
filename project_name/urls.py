e300a8d50d141278424706c7627bc24d7ed707b5
James Tauber <jtauber@jtauber.com>
2014-06-20 12:19:13 -0400

    homepage

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 
+from project_name.views import homepage
 from project_name.profiles.views import ProfileDetailView, ProfileEditView, ProfileListView
 
 
@@ -13,7 +13,7 @@ from .lookups import user_wiki_lookup, team_wiki_lookup
 
 urlpatterns = patterns(
     "",
-    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^$", homepage, name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
 
